73639111d6552ccb7fef21ba685315cb861f0b99
rossnayeJ <rossnaye.j@gmail.com>
2024-09-22 22:04:25 +0200

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -13,3 +13,6 @@ product_result = number1 * number2
 print(f'Addition of {number1} and {number2} is {sum_result}')
 print(f'Subtraction of {number1} and {number2} is {difference_result}')
 print(f'Multiplication of {number1} and {number2} is {product_result}')
+Addition of 10 and 5 is 15
+Subtraction of 10 and 5 is 5
+Multiplication of 10 and 5 is 50
